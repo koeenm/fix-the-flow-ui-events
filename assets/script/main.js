714e8569f570989846cee7fe017423d2df0378a7
koeenm <mkoeen2@gmail.com>
2023-12-11 11:58:05 +0100

    button 1 & 2

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,29 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+
+// Button 1
+let frontend = document.querySelector('a:nth-of-type(1)')
+
+frontend.addEventListener('click', function() {
+  frontend.classList.toggle('frontend')
+})
+
+let design = document.querySelector('a:nth-of-type(2)')
+
+design.addEventListener('dblclick', function() {
+  design.classList.toggle('design')
+})
+
+let and = document.querySelector('a:nth-of-type(3)')
+
+and.addEventListener('keydown' function(keyX) {
+  if (keyX.key x) {
+    
+  }
+    
+  }
+})
+
+
