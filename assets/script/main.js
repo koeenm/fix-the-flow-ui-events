fd36b78f0116a096f42f32d49076c546e3818b36
koeenm <mkoeen2@gmail.com>
2023-12-11 12:35:33 +0100

    button 3

--- assets/script/main.js
+++ assets/script/main.js
@@ -10,25 +10,27 @@ function jumpHandler() {
 
 // Button 1
 let frontend = document.querySelector('a:nth-of-type(1)')
-
+console.log(frontend); // hierdoor weet je of je script werkt, maar ik begrijp niet goed hoe je dat ziet
 frontend.addEventListener('click', function() {
-  frontend.classList.toggle('frontend')
+  frontend.classList.toggle('color-change');
 })
 
+// Button 2
 let design = document.querySelector('a:nth-of-type(2)')
 
 design.addEventListener('dblclick', function() {
-  design.classList.toggle('design')
+  design.classList.toggle('scale-up')
 })
 
+// Button 3
 let and = document.querySelector('a:nth-of-type(3)')
 
-and.addEventListener('keydown' function(keyX) {
-  if (keyX.key x) {
-    
-  }
-    
-  }
-})
+document.addEventListener('keydown', key2) //schrijf 'document' in plaats van de let (naam) want anders werkt de keydown niet goed
 
+// als je eerst de let (naam) schrijft dan werkt het event alleen wanneer je gefocust bent op de functie. Met document hoef je niet gefocust te zijn op de functie.
+  function key2(event) {
+    if (event.key === "2") {
+      and.classList.toggle("flip");
+    }
+  }
 
